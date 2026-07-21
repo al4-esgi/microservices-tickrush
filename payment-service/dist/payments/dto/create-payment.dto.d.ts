2a12e510597b3e8bd760d7c6224171e15f444f4c
Alexandru Rusescu <alexmonac13@gmail.com>
2026-07-21 16:09:12 +0200

    chore(payment-service): ignorer dist/ (artefacts de build)

--- payment-service/dist/payments/dto/create-payment.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreatePaymentDto {
-    reservationId: string;
-    amount: number;
-}
--- payment-service/dist/payments/payments.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Response } from 'express';
-import { CreatePaymentDto } from './dto/create-payment.dto';
-import { PaymentsService } from './payments.service';
-export declare class PaymentsController {
-    private readonly payments;
-    constructor(payments: PaymentsService);
-    create(dto: CreatePaymentDto, res: Response): void;
-    status(reservationId: string): string;
-}
